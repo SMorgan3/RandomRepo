d935f444eaf152475474577846bd51c52b5f951e
SMorgan3 <smorgan3@cocc.edu>
2017-10-03 12:34:03 -0700

    added features
    added button, text field, and altered output location

--- scripts/oct3.js
+++ scripts/oct3.js
@@ -3,8 +3,25 @@ function test() {
     alert("Hello World");
 }
 
-//Get text from input1 text box
+
 function readText() {
+    //Grab input element from document
     var input1 = document.getElementById("input1");
-    alert(input1);
+
+    //Store value from input1 to theText variable
+    var theText = input1.value;
+
+    //Demonstrate
+    alert("value is " + theText);
+   
+    //Make the value all uppercase
+    var upperCase = theText.toUpperCase();
+
+    //Grab output1 element
+    var output1 = document.getElementById("output1");
+
+    //Put uppercased input value into output1 field
+    output1.value = upperCase;
 }
+
+
